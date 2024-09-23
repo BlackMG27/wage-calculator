899646620e78038620f7d5a5c4c6340efddfb13e
BlackMG27 <oike279@gmail.com>
2024-09-23 16:02:08 -0500

    started on the setup

---
+++ src/calculator.js
@@ -0,0 +1,45 @@
+//set up the variables
+    //check for hourly  
+    //multiply by hours in bi-weekly, then by 26
+    //store bi-weekly 
+    //store yearly 
+    //store upfrontRaise
+    //store yearOneRaise 
+    //store yearTwoRaise
+
+    //store upHourly
+    //store upBiWeekly 
+    //store upYearly
+
+    //store yearOneHourly 
+    //store yearOneBiWeekly 
+    //store yearOneYearly 
+
+    //store yearTwoHourly 
+    //store yearTwoBiWeekly 
+    //store yearTwoYearly 
+
+    //raise = raise/100
+    // upFrontRaise = raise /100
+    //yearOneRaise = raise / 100
+    //yearTwoRaise = raise / 100
+
+    //grab the values 
+        //
+
+//upfront 
+    //multiply by upfrontRaise 
+        //upHourly = hourly + (hourly x upfrontRaise)
+        //upBiWeekly = bi-weekly + (bi-weekly x upfrontRaise)
+        //upYearly = yearly + (yearly x upfrontRaise)
+//year 1
+    ////multiply by yearOneRaise
+        //yearOneHourly = upHourly + (upHourly x yearOneRaise)
+        //yearOneBiWeekly = upBiWeekly + (upBiWeekly x yearOneRaise)
+        //yearOneYearly  = upYearly + (upYearly x yearOneRaise)
+//year 2
+    ////multiply by yearTwoRaise
+        //yearTwoHourly = yearOneHourly  + (yearOneHourly x yearTwoRaise)
+        //yearTwoBiWeekly = yearOneBiWeekly + (yearOneBiWeekly x yearTwoRaise)
+        //yearTwoYearly =  yearOneYearly + (yearOneYearly x yearTwoRaise)
+
